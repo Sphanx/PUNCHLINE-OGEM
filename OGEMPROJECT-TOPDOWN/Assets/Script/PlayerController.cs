c423cd267cded467f9b91e3fd0dc7f9b7aaf5081
Oğuzhan <oguzztrk1@gmail.com>
2024-01-04 17:32:23 +0300

    added char attack anim and rotation

--- OGEMPROJECT-TOPDOWN/Assets/Script/PlayerController.cs
+++ OGEMPROJECT-TOPDOWN/Assets/Script/PlayerController.cs
@@ -12,9 +12,7 @@ public class PlayerController : MonoBehaviour
     public float reduceStmOnDash;
     public float moveSpeed = 5f;
     public float dashForce;
-    public float thirstVar;
     public float hungerVar;
-    public float exhaustVar;
     public float staminaVar;
 
     [Space(20)]
@@ -22,16 +20,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] int Health = 100;
     [Tooltip("Hunger")]
     [SerializeField] int Hunger = 100;
-    [Tooltip("Thirst")]
-    [SerializeField] int Thirst = 100;
-    [Tooltip("Exhaust")]
-    [SerializeField] int Exhaust = 100;
     [Tooltip("Stamina")]
     [SerializeField] int Stamina = 100;
     public Slider staminaSlider;
-    public Slider thirstSlider;
     public Slider hungerSlider;
-    public Slider exhaustSlider;
     public Slider healthSlider;
 
 
@@ -40,53 +32,49 @@ public class PlayerController : MonoBehaviour
 
     #region private fields
     private bool isDashing = false;
-    private Vector2 movementInput;
+    public Vector2 movementInput
+    { get; set; }
+
+    private Vector2 scale;
     SurvivalBar health;
     SurvivalBar hunger;
-    SurvivalBar thirst;
-    SurvivalBar exhaust;
     SurvivalBar stamina;
 
     private float dashCooldown = 2f; 
     private float lastDashTime;
+    
     #endregion
 
     private void Start()
     {
         health = new SurvivalBar();
         hunger = new SurvivalBar();
-        thirst = new SurvivalBar();
-        exhaust = new SurvivalBar();
         stamina = new SurvivalBar();    
 
         health.FullValue = Health;
         hunger.FullValue = Hunger;
-        thirst.FullValue = Thirst;
-        exhaust.FullValue = Exhaust;
         stamina.FullValue = Stamina;
+
+        scale = transform.localScale;
 
     }
 
 
     void Update()
     {
-        // Hareket giriþini kullanarak karakteri hareket ettir
-        Vector2 movement = new Vector2(movementInput.x, movementInput.y) * moveSpeed * Time.deltaTime;
+        Vector2 movement = new Vector2(movementInput.x, movementInput.y) * moveSpeed * Time.deltaTime;  // assign movement info to vector2 movement
         transform.Translate(movement);
 
         DecreaseValue();
 
         staminaSlider.value = stamina.Bar;
-        thirstSlider.value = thirst.Bar;
         hungerSlider.value = hunger.Bar;
-        exhaustSlider.value = exhaust.Bar;
         healthSlider.value = health.Bar;
-
-        //Debug.Log("thirst: " + thirst.Bar + "hunger: " + hunger.Bar + "exhaust: " + exhaust.Bar);
-
+        
+        rotatePlayer();
     }
 
-    // Input System'un "Move" aksiyonunu dinlemek için bu metodunu kullanabiliriz
+    //Player movement
     public void Move(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
@@ -132,8 +120,6 @@ public class PlayerController : MonoBehaviour
     {
 
         DecreaseValue(hunger, hungerVar * Time.deltaTime);
-        DecreaseValue(thirst, thirstVar * Time.deltaTime);
-        DecreaseValue(exhaust, exhaustVar * Time.deltaTime);
 
         if (isDashing)
         {
@@ -149,5 +135,29 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
         stamina.Bar = Mathf.Clamp(stamina.Bar, 0f, Stamina);
+    }
+
+    public void rotatePlayer()
+    {
+
+        if(movementInput.x < 0)
+        {
+            scale.x = -1;
+            Debug.Log("char moving right " + movementInput);
+        }
+        else if(movementInput.x > 0)
+        {
+            scale.x = 1;
+            Debug.Log("char moving right " + movementInput);
+        }
+        else if(movementInput.y < 0){
+            scale.y = -1;
+        }
+        else if (movementInput.y > 0)
+        {
+            scale.y = 1;
+        }
+
+        transform.localScale = scale;
     }
 }
