550ca60a6c4871fa4c217edff1e3ba446409e9c2
Onur Ulaş Ballıkaya <onurulas4934@gmail.com>
2024-03-12 19:40:33 +0300

    last version

--- OGEMPROJECT-TOPDOWN/Assets/Script/EnemyISID/EnemyISID.cs
+++ OGEMPROJECT-TOPDOWN/Assets/Script/EnemyISID/EnemyISID.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class EnemyISID : MonoBehaviour
 {
-    
-    void Start()
+    public int bomberDamage;
+    [SerializeField] private PlayerController playerControllerScript;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.layer == 6)
+        {
+            playerControllerScript.TakeDamage(bomberDamage);
+            Destroy(gameObject);
+        }
     }
 
     
-    void Update()
-    {
-        
-    }
 
-    
 }
